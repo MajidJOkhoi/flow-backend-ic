95e65218d402bf9b815a3f94b2ef8f37948efb5f
Sayed Amjad Ali <shahsayedamjadali@gmail.com>
2024-08-12 17:07:55 +0500

    added error handling in LicenceKey

--- src/controller/licenceKey.controller.js
+++ src/controller/licenceKey.controller.js
@@ -2,16 +2,16 @@ import { LicenceKey } from "../model/LicenceKey.model.js"
 import { licenceKeyRoute } from "../routes/licenceKey.route.js"
 import { ApiError } from "../utlis/ApiError.js"
 
-const createLicenceKey=async(req,res)=>{
+const createLicenceKey=async(req,res,next)=>{
  const {key} =req.body
  if(!key){
-    throw new ApiError(400,"Key is required")
+    return next(new ApiError(400,"Key is required")) 
  }
 
  const Licencekey=await LicenceKey.create({key})
 
  if(!Licencekey){
-    throw new ApiError(400,"Error occur while creating licence key")
+    return next(new ApiError(400,"Error occur while creating licence key")) 
  }
 
     res.status(200).json({
@@ -21,21 +21,21 @@ const createLicenceKey=async(req,res)=>{
     })
 }
 
-const authKey=async(req,res)=>{
+const authKey=async(req,res,next)=>{
 
    const {key} =req.body
    if(!key){
-      throw new ApiError(400,"Key is required")
+      return next(new ApiError(400,"Key is required")) 
    }
   
    const Licencekey=await LicenceKey.findOne({key}).select("-createdAt -updatedAt")
   
    if(!Licencekey){
-      throw new ApiError(400,"Invalid Key")
+      return next(new ApiError(400,"Invalid Key")) 
    }
 
    if(Licencekey.status===true){
-      throw new ApiError(400,"this key is already in use")
+      return next(new ApiError(400,"this key is already in use")) 
    }
   
       res.status(200).json({
