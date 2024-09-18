4e75cc62895072ad45203ce75f6f5d5376665670
Sayed Amjad Ali <shahsayedamjadali@gmail.com>
2024-09-18 18:06:13 +0500

    made changes in leave controller2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-app.use(cors({origin:process.env.ORIGIN,credentials:true}))
+app.use(cors({origin:process.env.ORIGIN,credentials:true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Authorization', 'Content-Type'],
+}))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:"true",limit:"16kb"}))
 app.use(cookieParser())
