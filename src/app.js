d1c24bd2d0fa4c47a56b54f91cf6e64efd33597d
Sayed Amjad Ali <shahsayedamjadali@gmail.com>
2024-09-18 18:18:57 +0500

    made changes in leave controller3

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-app.use(cors({origin:process.env.ORIGIN,credentials:true,
+app.use(cors({origin:"https://test.taglogy.com",credentials:true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: ['Authorization', 'Content-Type'],
 }))
