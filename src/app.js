c3498017d9e116d2a65529c302accbb4b842e473
Sayed Amjad Ali <shahsayedamjadali@gmail.com>
2024-09-25 14:28:37 +0500

    change in cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-app.use(cors({origin:process.env.ORIGIN,credentials:true}))
+app.use(cors({sameSite:"none",path:"/",credentials:true,origin:process.env.ORIGIN}))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:"true",limit:"16kb"}))
 app.use(cookieParser())
