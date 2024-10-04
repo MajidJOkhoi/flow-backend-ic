7f6c24bf445d39b6e5ef1d5a2cd8dd22b0b97e77
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-04 15:42:28 +0500

    seond commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-app.use(cors({credentials:true,origin:process.env.ORIGIN}))
+app.use(cors({
+    origin:process.env.ORIGIN,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Include credentials if needed
+  }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:"true",limit:"16kb"}))
 app.use(cookieParser())
