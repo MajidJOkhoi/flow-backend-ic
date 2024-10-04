73f212a809e96bca9c9ebbbc3081250ffff01ce6
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-04 23:18:00 +0500

    changing in cors 2

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 import express from "express"
-import cors from "cors"
+
 import cookieParser from "cookie-parser"
 
 const app=express()
 
-app.use(cors({
-    origin:process.env.ORIGIN,
-    methods: ['GET,POST,PUT,DELETE,OPTIONS'],
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true // Include credentials if needed
-  }))
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:"true",limit:"16kb"}))
 app.use(cookieParser())
--- src/server.js
+++ src/server.js
@@ -2,13 +2,19 @@ import {config} from "dotenv"
 import { app } from "./app.js";
 import { dbConnection } from "./dbConnection.js/db.js";
 import { ApiError } from "./utlis/ApiError.js";
+import cors from "cors"
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
 config(
     {
         path:"./.env"
     }
 )
-
+app.use(cors({
+    origin:process.env.ORIGIN,
+    methods: ['GET,POST,PUT,DELETE,OPTIONS'],
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Include credentials if needed
+  }))
 app.use(errorMiddleware);
   
 
