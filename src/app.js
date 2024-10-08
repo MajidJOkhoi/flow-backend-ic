6710c885086013ed939f919f2ffdaff1233bbbe7
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-08 16:46:58 +0500

    access to web

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app=express()
 
 app.use(cors({
-    origin:"*", 
+    origin:["https://flow-ic-web.vercel.app","http://localhost:5173"], 
     credentials: true 
   }))
 app.use(express.json({limit:"16kb"}))
