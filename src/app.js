7fb0d76f7667377101f9fe0a890b0fc0e07a0652
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-08 10:21:24 +0500

    refresh problem solved12

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app=express()
 
 app.use(cors({
-    origin:"https://ic-flow-web.vercel.app",
+    origin:["https://ic-flow-web.vercel.app", "http://localhost:5173"],
     credentials: true 
   }))
 app.use(express.json({limit:"16kb"}))
