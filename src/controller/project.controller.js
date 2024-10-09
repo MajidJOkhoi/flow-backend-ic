26f1799f343b025f19554661a9af450272a248e8
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-09 13:51:41 +0500

    getMyProjects

--- src/controller/project.controller.js
+++ src/controller/project.controller.js
@@ -1,28 +1,59 @@
+import { assign } from "nodemailer/lib/shared/index.js";
 import { Project } from "../model/project.model.js";
 
+const create = async (req, res) => {
+  const { title, description, dueDate, status, priority } = req.body;
 
-const create=async (req,res)=>{
-    const { title, description,dueDate,status,priority } = req.body;
+  if (
+    [title, description, dueDate, status, priority].some(
+      (item) => item.trim() == ""
+    )
+  ) {
+    throw new ApiError(401, "All fields are require");
+  }
 
+  const project = await Project.create({
+    title,
+    description,
+    status,
+    priority,
+    dueDate,
+    createdBy: req.user._id,
+    companyName: req.user.companyId,
+  });
 
+  if (!project) {
+    throw new ApiError(400, "Error Occur While Creating project");
+  }
 
-    if ([ title, description,dueDate,status,priority].some((item) => item.trim() == "")) {
-      throw new ApiError(401, "All fields are require");
-    }
-  
- 
+  res.json({
+    sucess: true,
+    message: "Successfully project created",
+  });
+};
 
-      const project=await Project.create({title,description,status,priority,dueDate,createdBy:req.user._id,companyName:req.user.companyId})
-    
-      if (!project) {
-        throw new ApiError(400, "Error Occur While Creating project");
-      }
+const getMyProjects = async (req, res) => {
+  const { _id } = req.user;
+  const myProjects =await Project.aggregate([
+    { $match: { createdBy: _id } },
+    {
+      $project: {
+        title: 1,
+        description: 1,
+        dueDate: 1,
+        status: 1,
+        assignMember: 1,
+        attachments: 1,
+        priority: 1,
+      },
+    },
+  ]);
 
-      res.json({
-        sucess: true,
-        message: "Successfully project created",
-      });
-}
+  res.json({
+    sucess: true,
+    message: "successfully get all my projects",
+    myProjects
+  });
+};
 
-
-export {create}
+export { create, getMyProjects };
--- src/routes/project.route.js
+++ src/routes/project.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create } from "../controller/project.controller.js";
+import { create, getMyProjects } from "../controller/project.controller.js";
 import { auth } from "../middleware/auth.js";
 
  const projectRouter=Router()
 
  projectRouter.route("/create").post(auth,create)
+ projectRouter.route("/getMyProjects").get(auth,getMyProjects)
 
 
 
