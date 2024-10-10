a4a2254223c611820ae3cb8e8e48c94c48c61f9d
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-10 16:21:33 +0500

    cors updated

--- src/server.js
+++ src/server.js
@@ -2,22 +2,15 @@ import {config} from "dotenv"
 import { app } from "./app.js";
 import { dbConnection } from "./dbConnection.js/db.js";
 import { ApiError } from "./utlis/ApiError.js";
-import cors from "cors"
+
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
 config(
     {
         path:"./.env"
     }
 )
-// const corsOptions = {
-//     origin: '*', // Replace with your frontend's URL
-//     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-//     credentials: true, // Allow credentials like cookies to be sent
-//   };
-// app.use(cors(corsOptions))
 
 
-// app.options('*', cors(corsOptions));
 
 app.use(errorMiddleware);
   
