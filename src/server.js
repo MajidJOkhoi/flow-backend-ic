f34d0982d53f8ac214cb0d354768bf201ce88c23
MajidJOkhoi <majidalijkhio@gmail.com>
2024-10-10 16:17:42 +0500

    origin is seted

--- src/server.js
+++ src/server.js
@@ -9,15 +9,15 @@ config(
         path:"./.env"
     }
 )
-const corsOptions = {
-    origin: '*', // Replace with your frontend's URL
-    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-    credentials: true, // Allow credentials like cookies to be sent
-  };
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: '*', // Replace with your frontend's URL
+//     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+//     credentials: true, // Allow credentials like cookies to be sent
+//   };
+// app.use(cors(corsOptions))
 
 
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(errorMiddleware);
   
